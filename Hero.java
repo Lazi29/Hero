be29a22778ab5a7cb82f2a425f334289bd32e55a
Lazi29 <ramoslazara2021@icloud.com>
2024-11-14 03:14:41 +0000

    Hero

---
+++ Hero.java
@@ -0,0 +1,24 @@
+public class Hero{
+    public static void main(String[] args){
+        
+        String heroName = "Super-Girl";
+        String heroName2 = "Spider-Man";
+        String heroName3 = "Super-Man";
+
+        int powerLevel;
+        powerLevel = 100;
+        System.out.println("power Level = " + powerLevel);
+        int powerLevel2;
+        powerLevel2 = 80;
+        System.out.println("power Level  = " + powerLevel2);
+        int powerLevel3;
+        powerLevel3 = 101;
+        System.out.println("power Level = " + powerLevel3);
+
+        String heroAffiliation = "Justice League ";
+        String heroAffiliation2 = "Avengers ";
+        String heroAffiliation3 = "Justice League ";
+
+
+    }
+}
